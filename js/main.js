19601010a81c5402ef66cd34e45d0b4ef8a18068
George <istivf@gmail.com>
2019-06-19 19:05:08 +0300

    8. Личный проект: доверяй, но проверяй

--- js/main.js
+++ js/main.js
@@ -9,6 +9,15 @@ var mainPin = document.querySelector('.map__pin--main');
 var addressInput = document.querySelector('#address');
 var housingType = form.querySelector('#type');
 var minPrice = form.querySelector('#price');
+var timeIn = form.querySelector('#timein');
+var timeOut = form.querySelector('#timeout');
+
+window.onload = function () {
+  if (housingType.value === 'flat') {
+    minPrice.setAttribute('min', 1000);
+    minPrice.placeholder = '1000';
+  }
+};
 
 for (var a = 0; a < formFielsets.length; a++) {
   formFielsets[a].setAttribute('disabled', 'disabled');
@@ -65,6 +74,38 @@ mainPin.addEventListener('click', function () {
   }
 });
 
-if (housingType.value === 'bungalo') {
-  minPrice.setAttribute('min', 1);
-}
+housingType.addEventListener('input', function () {
+  if (housingType.value === 'bungalo') {
+    minPrice.setAttribute('min', 0);
+    minPrice.placeholder = '0';
+  } else if (housingType.value === 'flat') {
+    minPrice.setAttribute('min', 1000);
+    minPrice.placeholder = '1000';
+  } else if (housingType.value === 'house') {
+    minPrice.setAttribute('min', 5000);
+    minPrice.placeholder = '5000';
+  } else if (housingType.value === 'palace') {
+    minPrice.setAttribute('min', 10000);
+    minPrice.placeholder = '10000';
+  }
+});
+
+timeIn.addEventListener('input', function () {
+  if (timeIn.value === '12:00') {
+    timeOut.value = '12:00';
+  } else if (timeIn.value === '13:00') {
+    timeOut.value = '13:00';
+  } else if (timeIn.value === '14:00') {
+    timeOut.value = '14:00';
+  }
+});
+
+timeOut.addEventListener('input', function () {
+  if (timeOut.value === '12:00') {
+    timeIn.value = '12:00';
+  } else if (timeOut.value === '13:00') {
+    timeIn.value = '13:00';
+  } else if (timeOut.value === '14:00') {
+    timeIn.value = '14:00';
+  }
+});
