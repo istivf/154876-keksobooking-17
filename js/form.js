0b2c86e71fc0cf80fee9f4bc84fa2533aa9935e1
George <istivf@gmail.com>
2019-06-28 00:50:48 +0300

    12. Личный проект: модуляция

--- js/form.js
+++ js/form.js
@@ -20,38 +20,31 @@
   }
 
   housingType.addEventListener('input', function () {
-    if (housingType.value === 'bungalo') {
-      minPrice.setAttribute('min', 0);
-      minPrice.placeholder = '0';
-    } else if (housingType.value === 'flat') {
-      minPrice.setAttribute('min', 1000);
-      minPrice.placeholder = '1000';
-    } else if (housingType.value === 'house') {
-      minPrice.setAttribute('min', 5000);
-      minPrice.placeholder = '5000';
-    } else if (housingType.value === 'palace') {
-      minPrice.setAttribute('min', 10000);
-      minPrice.placeholder = '10000';
+    switch (housingType.value) {
+      case 'bungalo':
+        minPrice.setAttribute('min', 0);
+        minPrice.placeholder = '0';
+        break;
+      case 'flat':
+        minPrice.setAttribute('min', 1000);
+        minPrice.placeholder = '1000';
+        break;
+      case 'house':
+        minPrice.setAttribute('min', 5000);
+        minPrice.placeholder = '5000';
+        break;
+      case 'palace':
+        minPrice.setAttribute('min', 10000);
+        minPrice.placeholder = '10000';
+        break;
     }
   });
 
   timeIn.addEventListener('input', function () {
-    if (timeIn.value === '12:00') {
-      timeOut.value = '12:00';
-    } else if (timeIn.value === '13:00') {
-      timeOut.value = '13:00';
-    } else if (timeIn.value === '14:00') {
-      timeOut.value = '14:00';
-    }
+    timeOut.value = timeIn.value;
   });
 
   timeOut.addEventListener('input', function () {
-    if (timeOut.value === '12:00') {
-      timeIn.value = '12:00';
-    } else if (timeOut.value === '13:00') {
-      timeIn.value = '13:00';
-    } else if (timeOut.value === '14:00') {
-      timeIn.value = '14:00';
-    }
+    timeIn.value = timeOut.value;
   });
 })();
